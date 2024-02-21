4a1d35c241e1ca0f623c208216c91d7e4b144588
Kris Sert <kristjan.sert@gmail.com>
2024-02-21 17:36:31 +0000

    added stg fhv

---
+++ taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("string")) }} as dispatching_base_num,
+    {{ dbt.safe_cast("Affiliated_base_number", api.Column.translate_type("string")) }} as affiliated_base_number,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+
+    -- trip info
+    {{ dbt.safe_cast("SR_Flag", api.Column.translate_type("integer")) }} as sr_flag,
+    
+
+from {{ source('staging','fhv_tripdata') }}
+where --  with pickup time in year 2019
+    extract(year from pickup_datetime) == '2019' 
+
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
