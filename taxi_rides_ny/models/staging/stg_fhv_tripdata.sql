3230a22a8322c11d613d66de5711d5f0392fb85f
Kris Sert <kristjan.sert@gmail.com>
2024-02-21 22:30:53 +0000

    d

--- taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -13,7 +13,7 @@ select
     cast(dropOff_datetime as timestamp) as dropoff_datetime,
     
     -- trip info
-    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("string")) }} as dispatching_base_num,
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("string")) }} as dispatching_base_number,
     {{ dbt.safe_cast("Affiliated_base_number", api.Column.translate_type("string")) }} as affiliated_base_number,
     {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
     {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
